f5dfe7b1481c1078bde63220924483c65b285cd1
Lauren Harradine <lhharradine@gmail.com>
2023-02-01 12:44:30 +1100

    Did exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if (Number(x) > 0) {
+        let n1 = 0, n2 = 1, nextTerm;
+        for(i = 1; i < Number(x); i++) {
+            nextTerm = n1 + n2; 
+            n1 = n2; 
+            n2 = nextTerm; 
+        }
+        // console.log (nextTerm)
+        return n2
+    }
+    else return "OOPS"
 };
 
 // Do not edit below this line
