b0427ea2717d4f651f9ce4d18247d1c3fffbc73a
Lauren Harradine <lhharradine@gmail.com>
2023-02-01 11:54:56 +1100

    Did exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let strReversed = str.split("")
+                        .reverse()
+                        .join("")
+                        .toLowerCase()
+                        .replace(/[.,\/#!$%\^ &\*;:{}=\-_`~()]/g,"");
+   
+    str = str.toLowerCase().replace(/[.,\/#!$%\^ &\*;:{}=\-_`~()]/g,"")
+    
+    console.log(`Original string: ${str}`)
+    console.log(`Reversed string: ${strReversed}`)
+    return strReversed === str ? true : false
 };
 
 // Do not edit below this line
